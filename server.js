e13b1bbe255f4ad448e94b99d7284d735dcd6ea2
happytogether <happytogether.annie@gmail.com>
2025-05-18 22:14:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 
 const port = process.env.PORT || 8080;
 // Parse URL-encoded form data (from POST like `application/x-www-form-urlencoded`)
 app.use(express.urlencoded({ extended: true }));
+
+// sendFile will go here
+app.get('/select-store', function(req, res) {
+  res.sendFile(path.join(__dirname, '/hilife.html'));
+});
+
 
 app.post('/', (req, res) => {
   console.log('Full body:', req.body);             // Prints the whole object
