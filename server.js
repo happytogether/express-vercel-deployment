607360dc21032fcde551774a956f2f7e0d47bff8
happytogether <happytogether.annie@gmail.com>
2025-05-18 19:30:00 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 8080;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Subscribe to Arpan Neupane's channel");
+app.post('/product', (req, res) => {
+  const { ECCVS, price } = req.body;
+
+  console.log('Product Name:', ECCVS);
+  console.log('Product Price:', price);
+
+  res.send(`Received product: ${ECCVS} at $${price}`);
 });
 
 app.listen(port, () => {
